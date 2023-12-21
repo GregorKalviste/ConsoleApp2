4d0c0ed65ecbf8e4e78ebc316219ddc011ffc55c
Opilane TTHK <opilane@tthk.ee>
2023-12-21 14:51:45 +0200

    dcfef

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleApp2
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Linq.Expressions;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int i = 1;
+            int j = 1;
+
+            for (i = 5; 1 >= 1; i--)
+                for (j = 1; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+            Console.WriteLine(" ");
         }
-    }
-}
+     }
+   }
